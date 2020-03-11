3ab85f962b08f88bd9fc65cdb5127395530941a8
Makoto <teachermako@gmail.com>
2020-03-11 16:08:27 +0200

    Updates

---
+++ precache-manifest.977e302f1bbd80410a9417c2ac534da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c368e4eaa631a63434b1ea6cf42b152",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "a28a56497f9b29a90323",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.35c0e00d.chunk.css"
+  },
+  {
+    "revision": "75f0432f57812251fc81",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.9110930f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.9110930f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a28a56497f9b29a90323",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.7fbdc695.chunk.js"
+  },
+  {
+    "revision": "c7da9686ef9c69f25978",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.7dcdbe9d.js"
+  }
+]);
